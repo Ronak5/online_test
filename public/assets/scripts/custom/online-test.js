a45c11f489938a561750f40767a6930def30b893
ronak5 <ronaktaldar@yahoo.in>
2015-12-21 18:28:56 +0530

    added timer , Start test UI & js show hide effects

--- public/assets/scripts/custom/online-test.js
+++ public/assets/scripts/custom/online-test.js
@@ -3,7 +3,7 @@ var OnlineTest = function () {
     return {
         //main function to initiate the module
         init: function () {
-
+          $("#question_container_screen").hide();
             var getParams = function() {
                 var params = {},
                     pairs = document.URL.split('?')
@@ -57,9 +57,56 @@ var OnlineTest = function () {
                 });
             }
 
+
+          $("#start_test_button").click(function(){
+            bootbox.confirm("As soon as you click on OK , your time will start <br/> <ol><li>No negative marking</li><li>Time durartion is 30mins</li><li>Total 45 questions</li></ol><b style='color: red'>Note : Do not open any other tab or search in other window your test will be auto submitted & session will be expired.</b>", function (result) {
+              if (result == true) {
+                  show_question_screen();
+                  start_timer();
+                }
+            });
+          });
+
+          var show_question_screen = function(){
+            $("#welcome_screen").hide();
+            $("#question_container_screen").show();
+            start_timer();
+          }
+
+
+          var start_timer = function(){
+            console.log("timer")
+            var counter = 30;
+            $("#timer_container").html(counter);
+            var interval = setInterval(function() {
+              console.log("yes")
+              counter--;
+              // Display 'counter' wherever you want to display it.
+              console.log(counter)
+              $("#timer_container").html(counter);
+
+              if (counter > 20) {
+                $(".timer_body").removeClass('bg-danger')
+                $(".timer_body").removeClass('bg-warning')
+                $(".timer_body").addClass('bg-success')
+              }else if(counter > 10){
+                $(".timer_body").removeClass('bg-danger')
+                $(".timer_body").addClass('bg-warning')
+                $(".timer_body").removeClass('bg-success')
+              }else if(counter > 5){
+                $(".timer_body").addClass('bg-danger')
+                $(".timer_body").removeClass('bg-warning')
+                $(".timer_body").removeClass('bg-success')
+              }
+
+              if (counter == 0) {
+                // Display a login box
+                clearInterval(interval);
+              }
+            }, 60000);
+          }
+
         }
-
-
 
     };
 
